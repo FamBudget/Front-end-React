47a282fd8d9a7d1e9922d975d1be6f419eefb2d2
Kravchuk Anton <Kravch-anton@mail.ru>
2023-06-13 20:13:00 +0300

    small fix

--- src/pages/Operations/Operations.jsx
+++ src/pages/Operations/Operations.jsx
@@ -1,31 +1,18 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {Navigate} from "react-router-dom";
-import {setAuth} from "../../redux/reducers/AuthReducer";
 import styles from './Operations.module.scss'
 
 export const Operations = () => {
     const isAuth = useSelector((state) => state.auth.auth);
-    const dispatch = useDispatch();
-    const logout = () => {
-        dispatch(setAuth(null));
-        delete localStorage.token;
-        delete localStorage.email;
-    };
+
 
     return !isAuth ? (
         <Navigate to="/login"/>
     ) : (
         <div className={styles.container}>
 
-                {" "}
-                Hello
-                <button
-                    style={{width: 200, height: 50, background: "#023576"}}
-                    onClick={logout}
-                >
-                    Logout
-                </button>
+
         </div>
     );
 };
