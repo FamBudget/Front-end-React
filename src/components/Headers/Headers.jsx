a0f1a230bdfc9fe466a00160fb22f84e60c0a50d
Andrey Lisovskiy <mitolcore1234@gmail.com>
2023-06-23 21:10:02 +0300

    added expenses component

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -1,43 +1,37 @@
-import React from 'react'
-import {useDispatch} from "react-redux";
-import {setAuth} from "../../redux/reducers/AuthReducer";
-import styles from './Headers.module.scss'
-import {useLocation} from "react-router-dom";
-
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setAuth } from "../../redux/reducers/AuthReducer";
+import styles from "./Headers.module.scss";
+import { useLocation } from "react-router-dom";
 
 export const Headers = () => {
-    const dispatch = useDispatch();
-    const url = useLocation()
-    let title = ''
-    switch (url.pathname) {
-        case "/accounts" :
-            title = "Счета";
-            break;
-        case "/operations" :
-            title = "Операции"
-            break;
+  const dispatch = useDispatch();
+  const url = useLocation();
 
-    }
+  let title = "";
+  switch (url.pathname) {
+    case "/accounts":
+      title = "Счета";
+      break;
+    case "/operations":
+      title = "Операции";
+      break;
+  }
 
-    const logout = () => {
-        dispatch(setAuth(null));
-        localStorage.clear()
-    };
+  const logout = () => {
+    dispatch(setAuth(null));
+    localStorage.clear();
+  };
 
-    return <>
-        <div className={styles.header}>
-            <h1 className={styles.title}>{title}</h1>
-            <h1 className={styles.logo}>BudgetFamily</h1>
-            <button
-                style={{width: 50, height: 30, background: "#87d7ad"}}
-                onClick={logout}
-            >
-                Logout
-            </button>
-        </div>
-        <div className={styles.subHeader}>
-            <h1 >{title}</h1>
-
-        </div>
-    </>
-}
+  return (
+    <div className={styles.header}>
+      <h1>{title}</h1>
+      <button
+        style={{ width: 50, height: 30, background: "#87d7ad" }}
+        onClick={logout}
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
