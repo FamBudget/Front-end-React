00e593a66a95375377e00b7612c171ca52627bee
Andrey Lisovskiy <mitolcore1234@gmail.com>
2023-06-23 20:02:58 +0300

    rebase

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -3,7 +3,10 @@ import { Field, Form, Formik } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 import * as yup from "yup";
 
-import { fetchRegistration } from "../../redux/reducers/RegistrationReducer";
+import {
+  fetchRegistration,
+  setCurrency,
+} from "../../redux/reducers/RegistrationReducer";
 import { setAuth } from "../../redux/reducers/AuthReducer";
 
 import styles from "./RegistrationForm.module.scss";
@@ -76,7 +79,9 @@ export const RegistrationForm = ({ setIsOpenRegistration }) => {
           dispatch(fetchRegistration(values));
           if (status === "resolved") {
             dispatch(setAuth(values.email));
+            dispatch(setCurrency(values.currency));
             localStorage.email = values.email;
+            localStorage.currency = values.currency;
           }
           setSubmitting(false);
         }}
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export * from "./Accounts";
+export * from "./AuthPage";
+export * from "./Layouts/LayoutMain";
+export * from "./Layouts/LayoutAuth";
+export * from "./Operations";
