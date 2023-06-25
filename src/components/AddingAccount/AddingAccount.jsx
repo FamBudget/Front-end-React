c271d6db7fb3abaaddc19229b0d011bbe5b2ec2a
Andrey Lisovskiy <mitolcore1234@gmail.com>
2023-06-25 22:45:25 +0300

    reworked expenses component, send data to server

--- src/components/AddingAccount/AddingAccount.jsx
+++ src/components/AddingAccount/AddingAccount.jsx
@@ -1,101 +1,94 @@
-import React, {useRef} from "react";
-import {Field, Form, Formik} from "formik";
-import {useDispatch, useSelector} from "react-redux";
+import React, { useRef } from "react";
+import { Field, Form, Formik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./AddingAccount.module.scss";
-import {Button} from "../../components";
-import {DatePickerField, formatDate} from "../DatePickerFields/DatePickerFileds";
-import {addAccount} from "../../redux/reducers/AccountsReducer";
-import {SelectIcon} from "./SelectIcon";
+import { Button } from "../../components";
+import {
+  DatePickerField,
+  formatDate,
+} from "../DatePickerFields/DatePickerFileds";
+import { addAccount } from "../../redux/reducers/AccountsReducer";
+import { SelectIcon } from "./SelectIcon";
 
-const hours = new Date().getTimezoneOffset() / 60
+const hours = new Date().getTimezoneOffset() / 60;
 
 export function subtractHours(date) {
-    date.setHours(date.getHours() + hours);
-    return formatDate(date);
+  date.setHours(date.getHours() + hours);
+  return formatDate(date);
 }
 
-export const AddingAccount = ({setOpen}) => {
-    const forgetPasswordRef = useRef();
-    const dispatch = useDispatch()
+export const AddingAccount = ({ setOpen }) => {
+  const forgetPasswordRef = useRef();
+  const dispatch = useDispatch();
 
-    const currency = useSelector(state => state.Accounts?.data[0]?.currency)
-    var newDate = new Date()
+  const currency = useSelector((state) => state.accounts?.data[0]?.currency);
+  var newDate = new Date();
 
-    const dispatchData = (values) => {
-        const changedValues = {...values, createdOn: subtractHours(values.createdOn)}
-        dispatch(addAccount(changedValues))
+  const dispatchData = (values) => {
+    const changedValues = {
+      ...values,
+      createdOn: subtractHours(values.createdOn),
+    };
+    dispatch(addAccount(changedValues));
+  };
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <Formik
+          initialValues={{
+            createdOn: newDate,
+            currency: currency,
+            iconNumber: 0,
+            name: "",
+            startAmount: "",
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            dispatchData(values);
+            setSubmitting(false);
+            setOpen(false);
+          }}
+        >
+          {({ isSubmitting, errors, values }) => (
+            <Form ref={forgetPasswordRef} className={styles.form}>
+              <div className={styles.header}>
+                <h2>Добавить счет</h2>
+                <SelectIcon iconNumber={values.iconNumber} />
+              </div>
+              <label>Название счета</label>
+              <Field type="text" name="name" placeholder="Наличные" />
+              {errors.password && <p>{errors.password}</p>}
+              <label>Валюта счета</label>
+              <Field
+                type="text"
+                name="currency"
+                default={currency}
+                disabled={true}
+              />
+              <label>Начальный остаток</label>
+              <div className={styles.row}>
+                <Field type="text" name="startAmount" placeholder="0" />
+                <span>{currency}</span>
+              </div>
+              <label>Дата начального остатка</label>
+              <DatePickerField name="createdOn" />
 
-    }
-    return (
-        <>
-            <div className={styles.wrapper}>
-                <Formik
-                    initialValues={{
-                        createdOn: newDate,
-                        currency: currency,
-                        iconNumber: 0,
-                        name: '',
-                        startAmount: ''
-                    }}
-                    onSubmit={(values, {setSubmitting}) => {
-                        dispatchData(values)
-                        setSubmitting(false);
-                        setOpen(false)
-
-                    }}
-                >
-                    {({isSubmitting, errors, values}) => (
-                        <Form ref={forgetPasswordRef} className={styles.form}>
-                            <div className={styles.header}><h2>Добавить счет</h2><SelectIcon
-                                iconNumber={values.iconNumber}/></div>
-                            <label>Название счета</label>
-                            <Field
-                                type="text"
-                                name="name"
-                                placeholder="Наличные"
-                            />
-                            {errors.password && <p>{errors.password}</p>}
-                            <label>Валюта счета</label>
-                            <Field
-                                type="text"
-                                name="currency"
-                                default={currency}
-                                disabled={true}
-                            />
-                            <label>Начальный остаток</label>
-                            <div className={styles.row}><Field
-                                type="text"
-                                name="startAmount"
-                                placeholder="0"
-                            />
-                                <span>{currency}</span>
-                            </div>
-                            <label>Дата начального остатка</label>
-                            <DatePickerField name="createdOn"/>
-
-
-                            <div className={styles.buttons}>
-                                <div className={styles.WrapBtn} onClick={() => setOpen(false)}>
-                                    <Button
-                                        className={styles.cancelButton}
-                                        text="Отмена"
-                                    />
-                                </div>
-                                <div className={styles.WrapBtn}>
-                                    <Button
-                                        className={styles.submitButton}
-                                        isSubmitting={isSubmitting}
-                                        type="submit"
-                                        text="Ок"
-                                    />
-                                </div>
-                            </div>
-
-                        </Form>
-                    )}
-                </Formik>
-            </div>
-
-        </>
-    );
+              <div className={styles.buttons}>
+                <div className={styles.WrapBtn} onClick={() => setOpen(false)}>
+                  <Button className={styles.cancelButton} text="Отмена" />
+                </div>
+                <div className={styles.WrapBtn}>
+                  <Button
+                    className={styles.submitButton}
+                    isSubmitting={isSubmitting}
+                    type="submit"
+                    text="Ок"
+                  />
+                </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </>
+  );
 };
