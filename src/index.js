cb5a347609d576a148aa1e3fbad2fdd26505fee2
Kravchuk Anton <Kravch-anton@mail.ru>
2023-07-01 19:05:56 +0300

    changes router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { Provider } from "react-redux";
-
-import { store } from "./redux/store";
-
-import { router } from "./router";
-
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
 import "./styles/index.scss";
+import App from "./app";
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+      <React.StrictMode>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </React.StrictMode>
   </Provider>
 );
